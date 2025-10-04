0af3880bde5c4b565e6633b3c31d51f8b60f384b
turulko-oleksandr <sashaturulko@i.ua>
2025-10-04 22:10:53 +0300

    fix flake8

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -201,7 +201,7 @@ async def login_user(
 async def refresh_access_token(
         payload: TokenRefreshRequestSchema,
         db: AsyncSession = Depends(get_db),
-        settings =Depends(get_settings),
+        settings = Depends(get_settings),
         jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager)
 ):
     try:
